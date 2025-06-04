1f3a41fd1212c0a8c533fb92404a6eaeffafbcf4
Georgi Yordanov <georgimyordanov07@gmail.com>
2025-06-04 23:54:36 +0300

    First Commit!

---
+++ src/main.ts
@@ -0,0 +1,26 @@
+import express, { Request, Response } from "express";
+import dotenv from 'dotenv';
+import path from 'path';
+
+dotenv.config();
+const app = express();
+
+app.set('view engine', 'ejs');
+
+// Set the views directory
+app.set('views', path.join(__dirname, 'views'));
+
+// Set up routes
+app.get('/', (req: Request, res: Response) => {
+    res.render('index');
+});
+
+// Start the server
+const PORT = process.env.PORT;
+
+if (!process.env.PORT) throw new Error("No PORT value in .env");
+
+app.listen(PORT, () => {
+    console.log(`Server listening on port ${PORT}`);
+    console.log(`||| http://localhost:${PORT} |||`)
+});
