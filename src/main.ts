d16df9cddebf344af169a450f2032557e411bb5e
Georgi Yordanov <georgimyordanov07@gmail.com>
2025-06-08 23:56:02 +0300

    Made it pretty

--- src/main.ts
+++ src/main.ts
@@ -10,6 +10,9 @@ app.set('view engine', 'ejs');
 // Set the views directory
 app.set('views', path.join(__dirname, 'views'));
 
+// Serve static files from the public directory
+app.use(express.static(path.join(__dirname, '/public')));
+
 // Set up routes
 app.get('/', (req: Request, res: Response) => {
     res.render('index');
