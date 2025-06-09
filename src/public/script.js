354e3d952e8898ad77e5e1e00600a4600c2b787e
Georgi Yordanov <georgimyordanov07@gmail.com>
2025-06-09 22:53:54 +0300

    Stats, ads/sponsors, new font, new layout

---
+++ src/public/script.js
@@ -0,0 +1,58 @@
+// Generate numbers with each scroll
+
+let scrollingDownNumber = 100;
+let scrollingUpNumber = 1;
+// int limit 2147483647 - 10 zeros
+
+// Every 10ms, add the amount of lines generated in those 10ms to an array
+// That means in a second there will be 100 entries in the array
+// Every 100 lines (1 second), we remove the first element of the array
+// Then we add the new lines generated in the last 10ms
+let linesPerSeconds = [];
+let newLines = 0;
+function updateStats() {
+    if (linesPerSeconds.length < 100) {
+        linesPerSeconds.push(newLines);
+
+        let sum = linesPerSeconds.reduce((a, b) => a + b, 0);
+
+        document.getElementById('linesPerSecond').textContent = sum;
+        newLines = 0;
+    }
+    else {
+        linesPerSeconds.shift();
+    }
+
+    document.getElementById('totalLines').textContent = scrollingDownNumber + scrollingUpNumber;
+}
+
+window.addEventListener("wheel", (event) => {
+    const newNumber = document.createElement("p");
+
+    if (event.deltaY > 0) {
+        newNumber.textContent = scrollingDownNumber.toString();
+
+        const zeros = document.createElement("span");
+        zeros.textContent = "0".repeat(10 - scrollingDownNumber.toString().length);
+        zeros.style.color = "gray";
+        newNumber.insertBefore(zeros, newNumber.firstChild);
+
+        document.querySelector(".container").appendChild(newNumber);
+        scrollingDownNumber++;
+    } else if (event.deltaY < 0) {
+        // upscroll code
+        newNumber.textContent = scrollingUpNumber.toString();
+
+        const zeros = document.createElement("span");
+        zeros.textContent = "-" + "0".repeat(10 - scrollingUpNumber.toString().length);
+        zeros.style.color = "gray";
+        newNumber.insertBefore(zeros, newNumber.firstChild);
+
+        document.querySelector(".container").insertBefore(newNumber, document.querySelector(".container").firstChild);
+        scrollingUpNumber++;
+    }
+
+    newLines++;
+});
+
+setInterval(updateStats, 10);
